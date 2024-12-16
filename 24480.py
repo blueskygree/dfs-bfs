48e407207ab357cefcd91c8c653fff14c72b9953
blueskygree <133464320+blueskygree@users.noreply.github.com>
2024-12-16 15:57:09 +0900

    Create 24480.py

---
+++ 24480.py
@@ -0,0 +1,29 @@
+import sys
+input=sys.stdin.readline
+sys.setrecursionlimit(10000000)
+
+n,m,r=map(int,input().split())
+data=[[] for _ in range(n+1)]
+visited=[0]*(n+1)
+order=1
+
+for _ in range(m):
+    u,v=map(int,input().split())
+    data[u].append(v)
+    data[v].append(u)
+
+def dfs(r):
+    global order
+    visited[r]=order
+    order+=1
+    for i in data[r]:
+        if visited[i]==0:
+            dfs(i)
+
+for i in range(1,n+1):
+    data[i].sort(reverse=True)
+
+dfs(r)
+
+for i in range(1,n+1):
+    print(visited[i])
